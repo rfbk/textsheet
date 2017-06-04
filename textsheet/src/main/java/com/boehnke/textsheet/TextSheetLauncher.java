524c306e6ed857d134ffe0c79cf60081ec872ae9
rfbk <rfbk92@gmail.com>
2017-06-04 13:10:01 -0300

    initial commit for project

---
+++ textsheet/src/main/java/com/boehnke/textsheet/TextSheetLauncher.java
@@ -0,0 +1,15 @@
+package com.boehnke.textsheet;
+
+/**
+ * Launcher class for the TextSheet program
+ * @author Richard
+ *
+ */
+public class TextSheetLauncher {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
